6c0eed76e1f1d34cca2c293d6fe9bce6ae26e9ca
DaoQuangPhuong <daoquangphuong@gmail.com>
2020-03-20 17:43:12 +0700

    fix

--- services/dfo-chef/models/database/index.js
+++ services/dfo-chef/models/database/index.js
@@ -2,12 +2,24 @@ const sequelize = require('./sequelize');
 const Menu = require('./Menu');
 const Order = require('./Order');
 
+let retry = 0;
+
 const sync = async () => {
-  // sync database
-  await sequelize.sync();
-  // clean up database
-  const query = 'VACUUM FULL';
-  await sequelize.query(query, { type: sequelize.QueryTypes.SELECT });
+  try {
+    retry += 1;
+    // sync database
+    await sequelize.sync();
+    // clean up database
+    const query = 'VACUUM FULL';
+    await sequelize.query(query, { type: sequelize.QueryTypes.SELECT });
+  } catch (e) {
+    if (retry > 10) {
+      throw e;
+    }
+    console.error(e.message);
+    console.info('Retry Sync DB');
+    await sync();
+  }
 };
 
 module.exports = {
